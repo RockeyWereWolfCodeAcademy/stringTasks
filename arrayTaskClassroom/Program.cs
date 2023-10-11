6e0f2b177c34dd29c19ded7bd358bbca41f55ca7
RockeyWereWolf <mi71k9d7p@code.edu.az>
2023-10-11 11:20:20 +0400

    uploaded project

---
+++ arrayTaskClassroom/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace arrayTaskClassroom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //task1
+            //string word = "Hdllo";
+            //char letter = 'e';
+            //bool isLetterHere = false;
+            //for (int i = 0; i < word.Length; i++)
+            //{
+            //    if (word[i] == letter)
+            //    {
+            //        isLetterHere = true;
+            //        break;
+            //    }
+            //}
+            //Console.WriteLine(isLetterHere);
+            //task2
+            string sentence = "Ya xochu spat";
+            string findWord = "Ya";
+            int j = 0;
+            bool isWordHere = false;
+            for (int i = 0;!isWordHere; i++) 
+            {
+                if (j == findWord.Length-1)
+                {
+                    isWordHere = true;
+                    break;
+                }
+                if (sentence[i] == findWord[j])
+                {
+                    j++;
+                }
+            }
+            Console.WriteLine(isWordHere);
+        }
+    }
+}
